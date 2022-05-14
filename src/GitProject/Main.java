4924dd48a011366ece25c2ad49b426b1df7630d0
BasirKader <basirkader33@gmail.com>
2022-05-14 12:03:02 -0400

    2nd commit

--- src/GitProject/Main.java
+++ src/GitProject/Main.java
@@ -1,4 +1,11 @@
 package GitProject;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("im groot");
+        System.out.println("im groot");
+        System.out.println("im groot");
+
+    }
 }
